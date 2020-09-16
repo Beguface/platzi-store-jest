56b69450d784615aee9038169c9f56507364d6b6
beguface <beguface@gmail.com>
2020-09-15 22:52:28 -0400

    Initial commit

---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import { create } from 'react-test-renderer';
+import Header from '../../components/Header'; //Importar componente a probar.
+
+describe('<Header />', () => {
+  test('Render del Header', () => {
+    const header = shallow(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.length).toEqual(1);
+  });
+
+  test('Render del titulo', () => {
+    const header = mount(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Header Snapshot', () => {
+  test('Comprobar el Snapshot de Header', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.toJSON()).toMatchSnapshot();
+  });
+});
